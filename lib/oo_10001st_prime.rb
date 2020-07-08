f3da82e13e835cb9893ffe2c64fcd0a228772f24
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-08 15:20:15 +0000

    Automatically backed up by Learn

--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,21 @@
 # Implement your object-oriented solution here!
+require 'prime' 
+
+class Prime 
+  
+  def initialize(number) 
+    @number = number 
+  end 
+  
+  def number
+    prime_array = []
+    Prime.each(@number ** @number) do |prime|
+      prime_array << prime
+      if prime_array.length == @number 
+        break 
+      end
+    end 
+    prime_array.last
+  end
+
+end 
