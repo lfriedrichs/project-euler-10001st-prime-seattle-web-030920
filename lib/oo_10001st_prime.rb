d14811885b166a5ed0a8b598b99544ab36c0d9b0
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-08 15:27:33 +0000

    Automatically backed up by Learn

--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -4,7 +4,14 @@ require 'prime'
 class Prime 
   
   def number
-    prime_number_for(@number)
+    prime_array = []
+  Prime.each(@number ** @number) do |prime|
+    prime_array << prime
+    if prime_array.length == @number 
+      break 
+    end
+  end 
+  prime_array.last
   end
 
 end 
