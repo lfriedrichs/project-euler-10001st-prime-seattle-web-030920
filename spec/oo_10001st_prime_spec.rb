e8052f2ab1225ff441c4ee7673ad76862c6d93ae
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-08 15:31:35 +0000

    Automatically backed up by Learn

--- spec/oo_10001st_prime_spec.rb
+++ spec/oo_10001st_prime_spec.rb
@@ -1,17 +1,17 @@
 describe "Prime" do
   it 'returns 3 when 2 is passed in' do
-    expect(Prime.new(2).number).to eq(3)
+    expect(PrimeNumber.new(2).number).to eq(3)
   end
 
   it 'returns 7 when 4 is passed in' do
-    expect(Prime.new(4).number).to eq(7)
+    expect(PrimeNumber.new(4).number).to eq(7)
   end
 
   it 'returns 13 when 6 is passed in' do
-    expect(Prime.new(6).number).to eq(13)
+    expect(PrimeNumber.new(6).number).to eq(13)
   end
 
   it 'returns 104743 when 10001 is passed in' do
-    expect(Prime.new(10001).number).to eq(104743)
+    expect(PrimeNumber.new(10001).number).to eq(104743)
   end
 end
