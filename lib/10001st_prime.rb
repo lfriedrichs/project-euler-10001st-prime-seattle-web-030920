33915382a86b5496520f1253a4312247d0c548ba
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-08 15:32:25 +0000

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,13 @@
 # Implement your procedural solution here!
+require 'prime' 
+
+def prime_number_for(number) 
+  prime_array = []
+  Prime.each(number ** number) do |prime|
+    prime_array << prime
+    if prime_array.length == number 
+      break 
+    end
+  end 
+  prime_array.last
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,21 @@
 # Implement your object-oriented solution here!
+require 'prime' 
+
+class PrimeNumber
+  
+  def initialize(input)
+    @number = input
+  end
+  
+  def number
+    prime_array = []
+    Prime.each(@number ** @number) do |prime|
+      prime_array << prime
+      if prime_array.length == @number 
+        break 
+      end
+    end 
+    prime_array.last
+  end
+
+end 
